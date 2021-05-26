22fa013a9a58919b3cdf6f299f0ad026503fa4f3
johnrayignacio <49810970+johnrayignacio@users.noreply.github.com>
2021-05-26 10:09:11 +0800

    Add files via upload

--- rle/settings.py
+++ rle/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'l2n$42mni4%uu)hh9gxmwqg)zbh@@6rwp1)2c#3v0(1o#h=&if'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['eyenako.herokuapp.com', '127.0.0.1']
 
