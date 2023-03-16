ac6b4008046a169bb978262cb672d2b52ee28236
johnrayignacio <49810970+johnrayignacio@users.noreply.github.com>
2023-03-16 18:59:51 +0800

    Update settings.py

--- rle/settings.py
+++ rle/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'l2n$42mni4%uu)hh9gxmwqg)zbh@@6rwp1)2c#3v0(1o#h=&if'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['sinesclera.herokuapp.com', 'web-production-378a.up.railway.app' '127.0.0.1']
+ALLOWED_HOSTS = ['sinesclera.herokuapp.com', 'web-production-378a.up.railway.app', '127.0.0.1']
 
 
 # Application definition
